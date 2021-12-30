8ac732511b6f7a66b45543d0fdfe2fa7928b91d6
ccwukong <>
2021-12-30 08:28:29 +0000

    added mergesort

---
+++ sorting/__tests__/mergeSort.spec.js
@@ -0,0 +1,7 @@
+const mergeSort = require('../mergeSort');
+
+describe('Test mergeSort', ()=>{
+  it('Returns sorted arr.', ()=>{
+    expect(mergeSort([8,2,1,7,5,3,4,6])).toEqual([1,2,3,4,5,6,7,8]);
+  })
+})
