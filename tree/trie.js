b59d20b005f0d8be34c778495c73c2f374215f3d
ccwukong <ccwukong@gmail.com>
2022-01-02 21:59:48 +0800

    tbc

---
+++ tree/trie.js
@@ -0,0 +1,23 @@
+/**
+ * 02/02/2022 by Cheng(ccwukong)
+ *
+ *
+ */
+class Node {
+  constructor() {
+    this.dict = {};
+    this.isWord = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  add(w) {
+    let root = this.root;
+    for (let c of w) {
+    }
+  }
+}
